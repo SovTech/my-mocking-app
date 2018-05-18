6492e67411e15ce151eb6ec293a41becfbe050df
Jonathan Irwin <jonoiriwnrsa@gmail.com>
2018-05-18 15:52:39 +0200

    Other: removed some unneeded bloat

--- src/testing/graphql.ts
+++ src/testing/graphql.ts
@@ -8,16 +8,6 @@ type User @model {
   firstName: String!
   lastName: String!
   email: String! @isUnique
-  slackUserId: String @isUnique
-  avatarUrl: String
-  insultCreator: [Insult!]! @relation(name: "InsultCreator")
-  insultTarget: [Insult!]! @relation(name: "InsultTarget")
-  gamesCreated: [Game!]! @relation(name: "GameCreator")
-  whitePlayer0Games: [Game!]! @relation(name: "WhiteUser0")
-  whitePlayer1Games: [Game!]! @relation(name: "WhiteUser1")
-  blackPlayer0Games: [Game!]! @relation(name: "BlackUser0")
-  blackPlayer1Games: [Game!]! @relation(name: "BlackUser1")
-  roomSatusEntries: [RoomStatusEntry!]! @relation(name: "RoomStatusLogger")
 }
 
 type AllUsersQuery {
